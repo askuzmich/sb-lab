b0c6b46e3d4ad49d3c372121c8f11daf2b422139
askuzmich <askuzmich@gmail.com>
2024-07-06 17:16:51 +0300

    Удаление неиспользуемых модулей

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
