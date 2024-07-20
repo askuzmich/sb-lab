6804059bfa68cc2aad30dca6a3137269f6269b16
addamsv <googolcalculator@gmail.com>
2024-07-20 19:46:23 +0300

    feat: svg and file loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,21 @@ import { useTheme } from "./providers/ThemeProvider";
 import { classes } from "shared/lib/classNames/classes";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
+import { DarkThemeBtn } from "shared/ui/DarkThemeBtn";
+
+import Logo from "shared/assets/icons/logo.svg";
+import Img from "shared/assets/images/img.png";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={classes("app", {}, [theme])}>
       <Navbar />
       <AppRouter />
-      <button className="btn" onClick={toggleTheme}>
-        Theme
-      </button>
+      <Logo width={"300px"} />
+      {/* <Img /> */}
+      <DarkThemeBtn />
     </div>
   );
 };
