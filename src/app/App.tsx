7414cac67930ae5c88b1e0bf90af5abbb713ca9e
addamsv <googolcalculator@gmail.com>
2024-07-22 07:41:46 +0300

    feat: images and icons

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,14 @@
 import "./styles/index.scss";
 
-import { useTheme } from "./providers/ThemeProvider";
+import { Theme, useTheme } from "./providers/ThemeProvider";
 import { classes } from "shared/lib/classNames/classes";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { DarkThemeBtn } from "shared/ui/DarkThemeBtn";
 
 import Logo from "shared/assets/icons/logo.svg";
-import Img from "shared/assets/images/img.png";
+import GitLogo from "shared/assets/images/github-mark.png";
+import GitLogoLight from "shared/assets/images/github-mark-white.png";
 
 const App = () => {
   const { theme } = useTheme();
@@ -17,8 +18,16 @@ const App = () => {
       <Navbar />
       <AppRouter />
       <Logo width={"300px"} />
-      {/* <Img /> */}
+      <br />
       <DarkThemeBtn />
+      <br />
+      <br />
+      <img
+        src={theme === Theme.DARK ? GitLogoLight : GitLogo}
+        alt=""
+        width={30}
+        height={30}
+      />
     </div>
   );
 };
