0d4881b488850c0d154ec50e19098b5c07006619
addamsv <googolcalculator@gmail.com>
2024-07-24 14:38:36 +0300

    feat: new words

--- src/pages/AuthPage/ui/AuthPage.tsx
+++ src/pages/AuthPage/ui/AuthPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const AuthPage = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h2 className="App-link">REGISTRATION</h2>
-      {/* {auth} */}
+      <h2 className="App-link">{t("страница регистрации")}</h2>
     </div>
   );
 };
