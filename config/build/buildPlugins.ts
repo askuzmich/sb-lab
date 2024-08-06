4de966d9d9e84586017cad0f85361c9892bdbc44
addamsv <googolcalculator@gmail.com>
2024-08-06 20:29:38 +0300

    fix: bundle analyzer in prod

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,7 +8,7 @@ export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
@@ -19,9 +19,14 @@ export function buildPlugins({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    }),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
     })
   ];
+
+  if (isDev) {
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    }));
+  }
+
+  return plugins;
 }
