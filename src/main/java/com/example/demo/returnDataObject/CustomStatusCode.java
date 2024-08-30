1d8d5298db5b1856f56ff134667b274278de3b71
addamsv <googolcalculator@gmail.com>
2024-08-30 12:45:50 +0300

    add common return data

---
+++ src/main/java/com/example/demo/returnDataObject/CustomStatusCode.java
@@ -0,0 +1,17 @@
+package com.example.demo.returnDataObject;
+
+public class CustomStatusCode {
+
+    public static final int SUCCESS = 200; // Success
+
+    public static final int INVALID_ARGUMENT = 400; // Bad request, e.g., invalid parameters
+
+    public static final int UNAUTHORIZED = 401; // Username or password incorrect
+
+    public static final int FORBIDDEN = 403; // No permission
+
+    public static final int NOT_FOUND = 404;
+
+    public static final int INTERNAL_SERVER_ERROR = 500;
+
+}
